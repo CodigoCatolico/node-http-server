83f40987170b8eca448d243e258d651da8811c25
cesarFuhr <cesar.cara@protonmail.com>
2021-09-22 21:48:17 -0300

    add: http server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+  res.end('hello world!');
+});
+
+const port = 8080;
+console.log('server runing on port:', port);
+server.listen(port);
