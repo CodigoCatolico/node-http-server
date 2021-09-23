dc9280ea97be3d65340ff9b1e80fee50f92467db
cesarFuhr <cesar.cara@protonmail.com>
2021-09-22 21:53:49 -0300

    add: last changes

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function (req, res) {
-  res.end('hello world!');
+  const headers = req.headers;
+  const parsedURL = url.parse(req.url, true);
+  const query = parsedURL.query;
+  const path = parsedURL.path.split('/')[1];
+
+  let data = '';
+  req.on('data', chunk => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    res.setHeader('content-type', 'application/json');
+    res.writeHead(200);
+    res.end(
+      JSON.stringify({
+        headers: headers,
+        path: path,
+        query: query,
+        body: JSON.parse(data),
+      })
+    );
+  });
 });
 
 const port = 8080;
-console.log('server runing on port:', port);
+console.log('server listening on port:', port);
 server.listen(port);
